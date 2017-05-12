913b27817cae04d7a66b408038ad820fcff1a7f2
Chip Vaughn <cv223@saucelabs.com>
2017-05-12 10:11:02 -0400

    Update testBat.bat

--- testBat.bat
+++ testBat.bat
@@ -1,2 +1,2 @@
 @ECHO ON
-reg add "HKCU\software\microsoft\Windows\CurrrentVersion\Internet Settings\5.0" /v SyncMode5 /t REG_DWORD /d 00000003 /f
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings" /v SyncMode5 /t REG_DWORD /d 3 /f
